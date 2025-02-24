c48efa9431d39df94921d217be2d7fa6c7b31014
hootisman <david1zalewski@gmail.com>
2025-02-23 23:18:04 -0500

    working musicless jingle (once on thieving level)

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -5,12 +5,22 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.*;
+import net.runelite.api.widgets.ComponentID;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import javax.sound.midi.*;
+
+
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -28,20 +38,29 @@ public class ExamplePlugin extends Plugin
 	@Inject
 	private ExampleConfig config;
 
-
-	private int originalVolume;
-
-	private boolean checkingNextSong;
-
-	private int ticksChecked;
-
-
+	private int jingleTick;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		originalVolume = client.getMusicVolume();
-		resetCheckMusic();
+		byte[] data = 	client.getIndex(11).loadData(28, 0);
+		jingleTick = 0;
+
+		/*
+		File output = new File("./myfilewooo");
+
+		try ( FileOutputStream outputStream = new FileOutputStream(output); ) {
+
+			outputStream.write(data);  // Write the bytes and you're done.
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		*/
+
+		log.info("***Start");
+		log.info(Arrays.toString(data));
+		log.info("***End");
 	}
 
 	@Override
@@ -52,82 +71,76 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Subscribe
+	public void onStatChanged(StatChanged e){
+		log.info("*s* "+ e.toString());
+
+		Skill skill = e.getSkill();
+		int level = e.getLevel();
+		int listedLevel = PlayerStats.SKILL_LEVELS.get(skill);
+
+		//level never changed
+		if (listedLevel == level) return;
+
+		//if not during init phase & music is muted ...
+		if (listedLevel != -1 && client.getMusicVolume() == 0){
+			log.info("*s* leveled up while muted! ");
+			startJingle();
+		}
+
+		log.info("*s* old: " + listedLevel);
+		PlayerStats.SKILL_LEVELS.put(skill, level);
+		log.info("*s* new: " + PlayerStats.SKILL_LEVELS.get(skill));
+	}
+
+
+	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		log.info("CURRENT GAMESTATE: " + gameStateChanged.getGameState());
+		log.info("*GS* " + gameStateChanged.toString());
+	}
+	private void startJingle(){
+		if (client.getMusicVolume() == 0){
+			log.info("*j* unmuting for jingle...");
+			clientThread.invoke(() -> {
+				client.setMusicVolume(50);
+				jingleTick = 1;
+
+			});
+		}
 	}
 
+	private void endJingle(){
+		log.info("*j* jingle ended, muting...");
+		client.setMusicVolume(0);
+		jingleTick = 0;
+	}
 
 	@Subscribe
-	public void onClientTick(ClientTick tick){
-		clientThread.invokeLater(() -> {
-			if (checkingNextSong && ticksChecked < 120){
-				ticksChecked += 1;
-				log.info("Game tick " + ticksChecked);
-				printRequest();
-				testRequest();
-			} else if (checkingNextSong) {
-				resetCheckMusic();
-			} else if (client.getMusicVolume() == 0) {
-				checkMusic();
+	public void onGameTick(GameTick e){
+		if(jingleTick > 10){
+			endJingle();
+		}else if (jingleTick != 0){
+			jingleTick += 1;
+			log.info("*G* " + jingleTick);
+		}
+	}
+
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded e){
+		log.info("*W* " + e.toString());
+		if(e.getGroupId() == 193){
+			Widget icon  = client.getWidget(ComponentID.DIALOG_SPRITE_SPRITE);
+			if (icon != null && icon.getItemId() == 2996){
+				log.info("*W* This is a brimhaven ticket!");
+				startJingle();
 			}
-		});
-	}
-
-	@Subscribe
-	public void onVarbitChanged(VarbitChanged event){
-		if (event.getVarpId() == VarPlayer.MUSIC_VOLUME){
-
-			log.info("Music volume changed!" + event.getVarpId() + " = " + event.getValue());
-
 		}
+		//else if(e.getGroupId() == 233){
+		//	startJingle();
+		//}
 	}
 
-	@Subscribe(priority = -2)
-	public void onGameTick(GameTick tick){
 
-	}
-
-	@Subscribe
-	public void onPostClientTick(PostClientTick tick){
-		//log.info("vol " + client.getMusicVolume());
-
-	}
-
-	@Subscribe
-	public void onVolumeChanged(VolumeChanged event){
-		if (event.getType().equals(VolumeChanged.Type.MUSIC) && client.getMusicVolume() != 0){
-			//stop checking song
-			resetCheckMusic();
-		}
-	}
-
-	private void testRequest(){
-		List<MidiRequest> requests = client.getActiveMidiRequests();
-		for(MidiRequest request : requests){
-			log.info("---FOUND a next song: id: " + request.getArchiveId() + " ;is jingle? " + request.isJingle());
-			resetCheckMusic();
-		}
-	}
-
-	private void printRequest(){
-		List<MidiRequest> requests = client.getActiveMidiRequests();
-		for(MidiRequest request : requests){
-			log.info("midireq: id: " + request.getArchiveId() + " ;is jingle? " + request.isJingle());
-		}
-	}
-
-	public void checkMusic(){
-		//clientThread.invokeAtTickEnd(() -> {client.setMusicVolume(50);});
-		client.runScript(3966,30,10);	//[proc,settings_set_slider] script id 3966
-		checkingNextSong = true;
-		log.info("***CHECKING NEXT SONG");
-	}
-
-	public void resetCheckMusic(){
-		checkingNextSong = false;
-		ticksChecked = 0;
-	}
 
 	@Provides
 	ExampleConfig provideConfig(ConfigManager configManager)
